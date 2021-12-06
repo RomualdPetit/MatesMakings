2245f9f46afc0efe6f84f36343fe69073f4efac5
Roronoha69 <mbodje.justin@gmail.com>
2021-12-06 16:07:14 +0100

    CRUD/Model Game & Eent

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_113050) do
+ActiveRecord::Schema.define(version: 2021_12_06_140428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "plateform"
+    t.integer "start_time_hours"
+    t.integer "start_time_min"
+    t.date "start_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.string "plateform"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
