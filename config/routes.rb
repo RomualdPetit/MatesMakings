6cdcaa8eca4136ac7e883d65b4fb318ae794cb5f
RomualdPetit <romuol@hotmail.fr>
2021-12-06 17:44:52 +0100

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :events
   resources :games
+  get 'users/show'
   devise_for :users
   root'home#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
