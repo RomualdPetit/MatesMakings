d04456ece09752fba65fa6377c7429bb33d6a624
RomualdPetit <romuol@hotmail.fr>
2021-12-08 15:59:14 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,10 @@ Rails.application.routes.draw do
   resources :games
   devise_for :users
   resources :users
-<<<<<<< Updated upstream
   resources :communities, only: [:create, :destroy]
   
   root 'home#index'
   #post "events/new"
-=======
-  
-  root'home#index'
->>>>>>> Stashed changes
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
