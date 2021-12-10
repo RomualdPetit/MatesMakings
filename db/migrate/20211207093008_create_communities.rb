0dc4bc5a856ab80eedfe4a015d6cfc6b997fe35c
RomualdPetit <romuol@hotmail.fr>
2021-12-10 16:00:51 +0100

    fix schema

--- db/migrate/20211207093008_create_communities.rb
+++ db/migrate/20211207093008_create_communities.rb
@@ -1,15 +1,11 @@
 class CreateCommunities < ActiveRecord::Migration[5.2]
   def change
     create_table :communities do |t|
-
       
-      t.references :user, null: false, foreign_key: true
-      t.references :game, null: false, foreign_key: true
+      t.belongs_to :user, index: true
+      t.belongs_to :community, index: true
 
       t.timestamps
     end
-
-    add_index :communities, [:user_id, :game_id], unique: true
-
   end
 end
