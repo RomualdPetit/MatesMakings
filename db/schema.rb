9ff18def22f45228282a04bccb1f6693efade7d1
Roronoha69 <mbodje.justin@gmail.com>
2021-12-10 14:12:39 +0100

    fix bug database

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,12 @@ ActiveRecord::Schema.define(version: 2021_12_09_160436) do
   enable_extension "plpgsql"
 
   create_table "communities", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "game_id"
+    t.bigint "user_id", null: false
+    t.bigint "game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_communities_on_game_id"
+    t.index ["user_id", "game_id"], name: "index_communities_on_user_id_and_game_id", unique: true
     t.index ["user_id"], name: "index_communities_on_user_id"
   end
 
@@ -71,6 +72,7 @@ ActiveRecord::Schema.define(version: 2021_12_09_160436) do
     t.string "steam"
     t.string "availablity"
     t.decimal "note"
+    t.string "riot_games"
     t.string "uplay"
     t.string "psn"
     t.string "nintendo"
@@ -87,7 +89,6 @@ ActiveRecord::Schema.define(version: 2021_12_09_160436) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "communities", "games"
   add_foreign_key "communities", "users"
