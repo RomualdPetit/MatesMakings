34aada2aab2940c5a908882c9b7258eb9607cdd9
RomualdPetit <romuol@hotmail.fr>
2021-12-13 14:29:00 +0100

    fix heroku

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,7 +1,7 @@
 class AvatarsController < ApplicationController
     def create
         @user = User.find(params[:user_id])
-        @user.avatar.purge
+        #@user.avatar.purge
         @user.avatar.attach(params[:avatar])
         redirect_to(user_path(@user))
       end
