a6aef265fa719ad14a299b2bfcd6b640b8f8c677
RomualdPetit <romuol@hotmail.fr>
2021-12-17 00:40:24 +0100

    precompile

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,12 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.initialize_on_precompile = false
 module MatesMakings
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
