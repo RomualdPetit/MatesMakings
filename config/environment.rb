c9ca9d042bbe515bf5dcb23e42803d25ebb57c68
RomualdPetit <romuol@hotmail.fr>
2021-12-17 15:30:14 +0100

    actionmailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,5 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  user_name: ENV['SENDGRID_LOGIN'],
-  password: ENV['SENDGRID_PWD'],
-  domain: 'matesmaking@hotmail.com',
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  authentication:  :plain,
-  enable_starttls_auto: true
-}
+
 
