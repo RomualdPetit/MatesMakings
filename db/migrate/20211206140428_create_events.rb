d9fc71fd166721518f94e8bc2de64336c4b00347
Roronoha69 <mbodje.justin@gmail.com>
2021-12-17 11:38:21 +0100

    fix event creation bug

--- db/migrate/20211206140428_create_events.rb
+++ db/migrate/20211206140428_create_events.rb
@@ -7,7 +7,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.string :plateform
       t.integer :start_time_hours
       t.integer :start_time_min
-      t.integer :game_number
+      t.string :game_name
 
       t.date :start_date
 
