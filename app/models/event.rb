b8b231584b7208bdf63272a57331ad2b83a7c7ba
RomualdPetit <romuol@hotmail.fr>
2021-12-17 16:27:21 +0100

    game_name

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,7 @@ class Event < ApplicationRecord
     validates :title, presence: true, uniqueness: true, length: { minimum: 2, maximum:20, too_long: "%{count} characters is the maximum allowed" } 
     
     validates :description, presence: true, length: { minimum: 5, maximum:200 }
-    validates :game_name, presence: true
+    
     validates :plateform, presence: true
 
     validate :start_date_must_be_from_today
