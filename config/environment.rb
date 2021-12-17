175e94eb8c473a483c07dc2df621072118b4a957
Roronoha69 <mbodje.justin@gmail.com>
2021-12-17 15:53:34 +0100

    before crazy heroku moves

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,16 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+
+#config.action_mailer.delivery_method = :smtp
+
+#config.action_mailer.default_url_options = { host: 'http://localhost:3000/' }
+
+
 ActionMailer::Base.smtp_settings = {
-  user_name: ENV['SENDGRID_LOGIN'],
-  password: ENV['SENDGRID_PWD'],
-  domain: 'matesmaking@hotmail.com',
-  address: 'smtp.sendgrid.net',
+  user_name: "matesmakings@gmail.com",
+  password: "100code4ever",
+  address: 'smtp.gmail.com',
   port: 587,
   authentication:  :plain,
   enable_starttls_auto: true
