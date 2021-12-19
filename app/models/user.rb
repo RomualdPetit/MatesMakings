635bd61b989ac6f2cbb660db357f171bda9d87f0
RomualdPetit <romuol@hotmail.fr>
2021-12-19 16:09:13 +0100

    ActionMailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,5 +28,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-       
+        after_create :welcome_send
+
+        def welcome_send
+          UserMailer.welcome_email(self).deliver_now
+        end
+
 end
